84550241f546a80a20d5c398258e09d3a4ed1dd5
jxs714 <jxproductions@gmx.com>
2025-09-03 04:59:47 -0500

    Update urls.py

--- email_invites/urls.py
+++ email_invites/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views  # Comment this out temporarily
 
 app_name = "email_invites"
 
 urlpatterns = [
-    path("send-invitations/", views.InvitationSendView.as_view(), name="send_invitations"),
+    # path("send-invitations/", views.InvitationSendView.as_view(), name="send_invitations"),
 ]
