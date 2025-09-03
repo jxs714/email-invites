ad839fa9838a799a92d62bdd1f9de8540f19bf33
jxs714 <jxproductions@gmx.com>
2025-09-03 04:56:32 -0500

    Update forms.py

--- email_invites/forms.py
+++ email_invites/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 from pretalx.common.models import MailTemplate
 
-
 class InvitationForm(forms.Form):
     template = forms.ModelChoiceField(
         queryset=MailTemplate.objects.none(),
