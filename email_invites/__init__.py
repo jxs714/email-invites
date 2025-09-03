8babf98b4ffce47c8a7a31ad54facc28fddde269
jxs714 <jxproductions@gmx.com>
2025-09-03 02:54:13 -0500

    Update __init__.py

--- email_invites/__init__.py
+++ email_invites/__init__.py
@@ -1,1 +1,16 @@
-__version__ = "0.0.1"
+from django.utils.translation import gettext_lazy as _
+from pretalx.plugin import Plugin
+
+
+class PretalxPluginEmailInvites(Plugin):
+    name = _("Email Invites")
+    author = "Your Name"
+    description = _("Send mass email invitations using pretalx templates")
+    version = "0.0.1"
+
+    def ready(self):
+        from . import views  # noqa
+
+    def get_urls(self):
+        from . import urls
+        return urls.urlpatterns
