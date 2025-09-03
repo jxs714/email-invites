34617e5262e74ac3c1ebeb3335e4c335d685f501
jxs714 <jxproductions@gmx.com>
2025-09-03 05:13:17 -0500

    Delete email_invites/urls.py

--- email_invites/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "email_invites"
-
-urlpatterns = [
-    path("send-invitations/", views.InvitationSendView.as_view(), name="send_invitations"),
-]
