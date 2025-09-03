706161c9eb61cb7e4ac273b60c8a5e4440baae70
jxs714 <jxproductions@gmx.com>
2025-09-03 04:59:35 -0500

    Update __init__.py

--- email_invites/__init__.py
+++ email_invites/__init__.py
@@ -1,23 +1,17 @@
 from django.utils.translation import gettext_lazy as _
 
-try:
-    from pretalx.plugin import Plugin
-except ImportError:
-    # Fallback for when pretalx is not available (during build)
-    class Plugin:
-        pass
-
-class EmailInvitesPlugin(Plugin):
+class EmailInvitesPlugin:
     name = _("Email Invites")
     author = "JXS"
     description = _("Send mass email invitations using pretalx templates")
     version = "0.0.1"
 
     def ready(self):
-        # Don't import anything here that might cause circular imports
+        # Don't import anything here that might cause issues
         pass
 
     def get_urls(self):
+        # Import only when needed to avoid circular imports
         try:
             from . import urls
             return urls.urlpatterns
