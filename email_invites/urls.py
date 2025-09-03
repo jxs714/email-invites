f24192058e5aaa9963348d45b9ec49950cb109e7
jxs714 <jxproductions@gmx.com>
2025-09-03 03:00:33 -0500

    Update urls.py

--- email_invites/urls.py
+++ email_invites/urls.py
@@ -1,14 +1,13 @@
+from django.urls import path
 
-from django.urls import re_path
+from . import views
 
-from pretalx.event.models.event import SLUG_REGEX
-
-from .views import EmailInvitesSettingsView
+app_name = "email_invites"
 
 urlpatterns = [
-    re_path(
-        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/email_invites/$",
-        EmailInvitesSettingsView.as_view(),
-        name="settings",
+    path(
+        "event/<str:event>/send-invitations/",
+        views.InvitationSendView.as_view(),
+        name="send_invitations",
     ),
 ]
